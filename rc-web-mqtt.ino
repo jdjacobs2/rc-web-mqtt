530b3b9342a74b2ba6bfe3395a0b808971e33011
jdjacobs2 <jim@jacobshome.com>
2022-01-04 16:01:29 -0600

    Change to ESPAsyncWebServer

--- rc-web-mqtt.ino
+++ rc-web-mqtt.ino
@@ -5,20 +5,20 @@
 #include <PubSubClient.h> // Allows us to connect to, and publish to the MQTT broker
 #include <ESP8266mDNS.h>
 #include <RCSwitch.h>
-#include <ESP8266WebServer.h>
+// #include <ESP8266WebServer.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+// Create a webserver object that listens for HTTP request on port 80
 
- // Create a webserver object that listens for HTTP request on port 80
-ESP8266WebServer server(80);
+RCSwitch mySwitch = RCSwitch(); // Initialize RCSwitch (radio freq) library
 
-RCSwitch mySwitch = RCSwitch();  // Initialize RCSwitch (radio freq) library
-
-void handleRoot();              // function prototypes for HTTP handlers
+void handleRoot(); // function prototypes for HTTP handlers
 void handleNotFound();
 
 const char *location = "rc";
 const char *ssid = "windsong";
 const char *wifi_password = "fubsey00";
-long now ;
+long now;
 long lastTime;
 
 // Don't change the function below. This functions connects your ESP8266 to your router
@@ -52,10 +52,10 @@ WiFiClient espClient;
 // PubSubClient client(mqtt_server, 1883, wifiClient); // 1883 is the listener port for the Broker
 PubSubClient client(espClient);
 
-// This functions is executed when some device publishes a message to a topic 
+// This functions is executed when some device publishes a message to a topic
 // that your ESP8266 is subscribed to
-// Change the function below to add logic to your program, so when a device 
-// publishes a message to a topic that your ESP8266 is subscribed you can 
+// Change the function below to add logic to your program, so when a device
+// publishes a message to a topic that your ESP8266 is subscribed you can
 // actually do something
 
 void callback(String topic, byte *message, unsigned int length)
@@ -73,8 +73,7 @@ void callback(String topic, byte *message, unsigned int length)
   // Serial.println();
 }
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
-
+// Feel free to add more if statements to control more GPIOs with MQTT
 
 // This functions reconnects your ESP8266 to your MQTT broker
 // Change the function below if you want to subscribe to more topics with your ESP8266
@@ -114,7 +113,11 @@ void reconnect()
   }
 }
 
-
+AsyncWebServer server(80);
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "404: Not Found");
+};
 
 void setup()
 {
@@ -130,25 +133,23 @@ void setup()
   client.setCallback(callback);
   Serial.println("");
 
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(200, "text/plain", "Hello World AsyncWebServer"); });
 
   // Setup HTTP server
-  // server.on("/", handleRoot);               // Call the 'handleRoot' function when a client requests URI "/"
-  // server.onNotFound(handleNotFound);        // When a client requests an unknown URI (i.e. something other than "/"), call function "handleNotFound"
-  SPIFFS.begin();
-  server.onNotFound([]() {
-    if (!handleFileRead(server.uri()))
-      server.send(404, "text/plain", "404: Not Found");
-  });
-  server.begin(); // Actually start the server
+  server.onNotFound(notFound);
+  server.begin();
   Serial.println("HTTP server started");
 
-  mySwitch.enableReceive(4);  // Receiver on interrupt 0 => that is pin #2
+  SPIFFS.begin();
+
+  mySwitch.enableReceive(4); // Receiver on interrupt 0 => that is pin #2
 }
 
 void loop()
 {
 
-  server.handleClient();  //Listen for HTTP requests
+  // server.handleClient(); // Listen for HTTP requests
 
   // if (!client.connected())   // Establish MQTT listner
   // {
@@ -157,55 +158,63 @@ void loop()
   // if (!client.loop())
   //   client.connect("ESP8266RC");
 
-
-
   // Serial.println('In main loop before client.publish');
   // client.publish("rc/read", "From ESP8622");
 
-  if (mySwitch.available()) {
+  if (mySwitch.available())
+  {
     Serial.println("my switch is available");
     output(mySwitch.getReceivedValue(), mySwitch.getReceivedBitlength(), mySwitch.getReceivedDelay(), mySwitch.getReceivedRawdata(), mySwitch.getReceivedProtocol());
     mySwitch.resetAvailable();
   }
 
-//   if (millis() - lastTime >= 1000)
-//   {
-//     Serial.println("in millis for mySwitch");
-//     lastTime = millis();
+  //   if (millis() - lastTime >= 1000)
+  //   {
+  //     Serial.println("in millis for mySwitch");
+  //     lastTime = millis();
 
-//   }
+  //   }
 
   // delay(1000);
 }
 
-bool handleFileRead(String path) { // send the right file to the client (if it exists)
-  Serial.println("handleFileRead: " + path);
-  if (path.endsWith("/")) path += "index.html";          // If a folder is requested, send the index file
-  Serial.print("path is:  ");
-  Serial.println(path);
-  String contentType = getContentType(path); // Get the MIME type
-  String pathWithGz = path + ".gz";
-  if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path)) { // If the file exists, either as a compressed archive, or normal
-    if (SPIFFS.exists(pathWithGz))                         // If there's a compressed version available
-      path += ".gz";                                         // Use the compressed verion
-    File file = SPIFFS.open(path, "r");                    // Open the file
-    size_t sent = server.streamFile(file, contentType);    // Send it to the client
-    file.close();                                          // Close the file again
-    Serial.println(String("\tSent file: ") + path);
-    return true;
-  }
-  Serial.println(String("\tFile Not Found: ") + path);   // If the file doesn't exist, return false
-  return false;
-}
+// bool handleFileRead(String path)
+// { // send the right file to the client (if it exists)
+//   Serial.println("handleFileRead: " + path);
+//   if (path.endsWith("/"))
+//     path += "index.html"; // If a folder is requested, send the index file
+//   Serial.print("path is:  ");
+//   Serial.println(path);
+//   String contentType = getContentType(path); // Get the MIME type
+//   String pathWithGz = path + ".gz";
+//   if (SPIFFS.exists(pathWithGz) || SPIFFS.exists(path))
+//   {                                                     // If the file exists, either as a compressed archive, or normal
+//     if (SPIFFS.exists(pathWithGz))                      // If there's a compressed version available
+//       path += ".gz";                                    // Use the compressed verion
+//     File file = SPIFFS.open(path, "r");                 // Open the file
+//     size_t sent = server.streamFile(file, contentType); // Send it to the client
+//     file.close();                                       // Close the file again
+//     Serial.println(String("\tSent file: ") + path);
+//     return true;
+//   }
+//   Serial.println(String("\tFile Not Found: ") + path); // If the file doesn't exist, return false
+//   return false;
+// }
 
-String getContentType(String filename) { // convert the file extension to the MIME type
-  if (filename.endsWith(".html")) return "text/html";
-  else if (filename.endsWith(".css")) return "text/css";
-  else if (filename.endsWith(".js")) return "application/javascript";
-  else if (filename.endsWith(".ico")) return "image/x-icon";
-  else if (filename.endsWith(".gz")) return "application/x-gzip";
-  return "text/plain";
-}
+// String getContentType(String filename)
+// { // convert the file extension to the MIME type
+//   if (filename.endsWith(".html"))
+//     return "text/html";
+//   else if (filename.endsWith(".css"))
+//     return "text/css";
+//   else if (filename.endsWith(".js"))
+//     return "application/javascript";
+//   else if (filename.endsWith(".ico"))
+//     return "image/x-icon";
+//   else if (filename.endsWith(".gz"))
+//     return "application/x-gzip";
+//   return "text/plain";
+// }
 
 // void handleRoot() {
 //   server.send(200, "text/plain", "Hello world!");   // Send HTTP status 200 (Ok) and send some text to the browser/client
